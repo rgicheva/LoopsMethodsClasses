26549d653b4d78db5ca600970f0bbc44dcd1446a
Radka Gicheva <radkagicheva@gmail.com>
2014-09-15 16:03:39 -0500

    first files added

---
+++ src/Lection3/_2_Generate3LetterWords.java
@@ -0,0 +1,10 @@
+package Lection3;
+
+public class _2_Generate3LetterWords {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/Lection3/_3_SymmetricNumbersInRange.java
@@ -0,0 +1,30 @@
+package Lection3;
+
+import java.util.Scanner;
+
+public class _3_SymmetricNumbersInRange {
+
+	public static void main(String[] args) {
+		System.out.print("Please enter two number separeted by space:");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+       
+        String firstNum = "";
+        String secondNum = "";
+        String currentNum = "";
+
+        for (int i = a; i <= b; i++) { 
+               
+                currentNum = Integer.toString(i);
+               
+                firstNum = currentNum.substring(0, currentNum.length()/2);
+                secondNum = currentNum.substring(currentNum.length()/2+currentNum.length()%2, currentNum.length());
+               
+                if (firstNum.equals(secondNum)) {
+                        System.out.print(currentNum + " ");
+                }
+        }
+	}
+
+}
